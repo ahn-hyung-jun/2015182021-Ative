8fbfd3941f1c70374a8ef805c90ebaa4e41efe64
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-09-17 19:59:00 +0900

    케릭터 이동 뼈대작성
    케릭터 이동함수를 10개로 나누어 만듬

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -2,8 +2,48 @@ from pico2d import *
 
 open_canvas()
 
-def move_character():
+def move_1_to_2():
     pass
+
+def move_2_to_3():
+    pass
+
+def move_3_to_4():
+    pass
+
+def move_4_to_5():
+    pass
+
+def move_5_to_6():
+    pass
+
+def move_6_to_7():
+    pass
+
+def move_7_to_8():
+    pass
+
+def move_8_to_9():
+    pass
+
+def move_9_to_10():
+    pass
+
+def move_10_to_1():
+    pass
+
+def move_character():
+    move_1_to_2()
+    move_2_to_3()
+    move_3_to_4()
+    move_4_to_5()
+    move_5_to_6()
+    move_6_to_7()
+    move_7_to_8()
+    move_8_to_9()
+    move_9_to_10()
+    move_10_to_1()
+
 
 while True:
     move_character()
