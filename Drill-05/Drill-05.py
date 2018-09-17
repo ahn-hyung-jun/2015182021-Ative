bb6873d1c4a69c310f543650567153569fb23ab9
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-09-17 20:16:48 +0900

    1번점에서 2번점으로 이동을 완료
    두 점의 좌표를 이용해 기울기를 구했음

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -2,8 +2,26 @@ from pico2d import *
 
 open_canvas()
 
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+
 def move_1_to_2():
-    pass
+    x1, y1, x2, y2 = 203,535,132,243
+    x_plus=x2-x1
+    y_plus=y2-y1
+    r=y_plus//x_plus
+    frame = 0
+    while(x1>x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 -= 5
+        y1 -=5*r
+        delay(0.05)
+        get_events()
 
 def move_2_to_3():
     pass
