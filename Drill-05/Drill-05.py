497d07f3ac365d325c51a6b794e2b6b32d393438
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-09-17 20:40:50 +0900

    4번점에서 5번점 이동

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -58,10 +58,38 @@ def move_3_to_4():
         get_events()
 
 def move_4_to_5():
-    pass
+    x1, y1, x2, y2 = 477, 203, 715, 136
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 < x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 += 5
+        y1 += 5 * r
+        delay(0.05)
+        get_events()
 
 def move_5_to_6():
-    pass
+    x1, y1, x2, y2 = 715, 136, 316, 225
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 > x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 -= 5
+        y1 -= 5 * r
+        delay(0.05)
+        get_events()
 
 def move_6_to_7():
     pass
@@ -81,8 +109,8 @@ def move_10_to_1():
 def move_character():
     #move_1_to_2()
     #move_2_to_3()
-    move_3_to_4()
-    move_4_to_5()
+    #move_3_to_4()
+    #move_4_to_5()
     move_5_to_6()
     move_6_to_7()
     move_7_to_8()
