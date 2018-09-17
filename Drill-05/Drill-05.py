df03076733ff3478d00355b655bb8dba7fe359f5
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-09-17 20:51:29 +0900

    7,8,9,10번점 이동
    까먹고 안올려서 한번에 올림

--- Drill-05/Drill-05.py
+++ Drill-05/Drill-05.py
@@ -109,16 +109,72 @@ def move_6_to_7():
         get_events()
 
 def move_7_to_8():
-    pass
+    x1, y1, x2, y2 = 510, 92, 692, 518
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 < x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 += 5
+        y1 += 5 * r
+        delay(0.05)
+        get_events()
 
 def move_8_to_9():
-    pass
+    x1, y1, x2, y2 = 692, 518, 682, 336
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 > x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 -= 2
+        y1 -= 2 * r
+        delay(0.05)
+        get_events()
 
 def move_9_to_10():
-    pass
+    x1, y1, x2, y2 = 682, 336, 712, 349
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 < x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 += 5
+        y1 += 5 * r
+        delay(0.05)
+        get_events()
 
 def move_10_to_1():
-    pass
+    x1, y1, x2, y2 = 712, 349, 203, 535
+    x_plus = x2 - x1
+    y_plus = y2 - y1
+    r = y_plus / x_plus
+    frame = 0
+    while (x1 < x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 += 5
+        y1 += 5 * r
+        delay(0.05)
+        get_events()
 
 def move_character():
     #move_1_to_2()
@@ -126,9 +182,9 @@ def move_character():
     #move_3_to_4()
     #move_4_to_5()
     #move_5_to_6()
-    move_6_to_7()
-    move_7_to_8()
-    move_8_to_9()
+    #move_6_to_7()
+    #move_7_to_8()
+    #move_8_to_9()
     move_9_to_10()
     move_10_to_1()
 
