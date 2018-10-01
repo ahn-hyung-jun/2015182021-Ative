00a3ee6f1d7ad6a3895fd5278fd6744134d30078
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-10-01 17:03:07 +0900

    캔버스 생성
    KPU를 화면에 띄움
    랜덤 20개좌표 생성

--- Drill-07/Drill-07.py
+++ Drill-07/Drill-07.py
@@ -3,13 +3,18 @@ import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-def draw_line(p1, p2):
+def move_character(p1, p2):
     for i in range(0,100+1,2):
         t = i / 100
         x = (1-t)*p1[0]+t*p2[0]
         y = (1-t)*p1[1]+t*p2[1]
-
-
+        frame = 0
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
 
 
 
@@ -23,11 +28,12 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
-hand_arrow = load_image('hand_arrow.png')
 
 
 
 while True:
-    draw_line(points[n-1], points[n])
+    move_character(points[n-1], points[n])
     n = (n+1)%size
 
+
+close_canvas()
