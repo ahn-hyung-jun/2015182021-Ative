fa6b510eed4ca996394fc4597e4dcfa45a5d7ca8
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-10-04 17:08:08 +0900

    혹시나 가능할까싶어 %연산으로 루프를 돌려보자

--- Drill-07/Drill-07.py
+++ Drill-07/Drill-07.py
@@ -6,37 +6,16 @@ import random
 def move_character(x,y):
     pass
 
-def draw_curve_5_points(p1, p2, p3, p4, p5):
+def draw_curve_5_points(points, size):
 
-    # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        move_character(x, y)
-
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        move_character(x, y)
-
-    # draw p3-p4
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p2[0] + (3*t**3 - 5*t**2 + 2)*p3[0] + (-3*t**3 + 4*t**2 + t)*p4[0] + (t**3 - t**2)*p5[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p2[1] + (3*t**3 - 5*t**2 + 2)*p3[1] + (-3*t**3 + 4*t**2 + t)*p4[1] + (t**3 - t**2)*p5[1])/2
-        move_character(x, y)
-
-    # draw p4-p5
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p3[0] + (-4 * t ** 2 + 4 * t) * p4[0] + (2 * t ** 2 - t) * p5[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p3[1] + (-4 * t ** 2 + 4 * t) * p4[1] + (2 * t ** 2 - t) * p5[1]
-        move_character(x, y)
-
-
+    for i in range(0,size):
+       for i in range(0, 100, 2):
+           t = i / 100
+           x = ((-t ** 3 + 2 * t ** 2 - t) * points[i%11][0] + (3 * t ** 3 - 5 * t ** 2 + 2) * points[(i+1)%11][0] + (
+                       -3 * t ** 3 + 4 * t ** 2 + t) * points[(i+2)%11][0] + (t ** 3 - t ** 2) * points[(i+3)%11][0]) / 2
+           y = ((-t ** 3 + 2 * t ** 2 - t) * points[i%11][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * points[(i+1)%11][1] + (
+                       -3 * t ** 3 + 4 * t ** 2 + t) * points[(i+2)%11][1] + (t ** 3 - t ** 2) * points[(i+3)%11][1]) / 2
+           move_character(x, y)
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
@@ -49,9 +28,9 @@ p2 = (400,350)
 p3 = (300,-300)
 p4 = (-200,-200)
 
-
-points = [(random.randint(-600,600),random.randint(-500,500)) for n in range(10)]
+size=10
+points = [(random.randint(-600,600),random.randint(-500,500)) for n in range(size)]
 while True:
- draw_curve_5_points(p1, p2, p3, p4, p1)
+ draw_curve_5_points(points,size)
 
 close_canvas()
