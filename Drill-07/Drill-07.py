ed679b03c84e2ef47571f3831c2bdca1d8270a93
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-10-04 17:15:28 +0900

    일단 turtle 이동부터 제출
    2015182021 안형준
    commit : 7회https://youtu.be/C_YEGQxbzMg
    
    이쁘게 만드려다 망했다...ㅠㅠ

--- Drill-07/Drill-07.py
+++ Drill-07/Drill-07.py
@@ -3,8 +3,17 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 import random
 
 
+
 def move_character(x,y):
-    pass
+    clear_canvas()
+    global frame
+    frame=0
+    frame = frame % 8
+    frame = frame + 1
+
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+    update_canvas()
 
 def draw_curve_5_points(points, size):
 
@@ -16,6 +25,7 @@ def draw_curve_5_points(points, size):
            y = ((-t ** 3 + 2 * t ** 2 - t) * points[i%11][1] + (3 * t ** 3 - 5 * t ** 2 + 2) * points[(i+1)%11][1] + (
                        -3 * t ** 3 + 4 * t ** 2 + t) * points[(i+2)%11][1] + (t ** 3 - t ** 2) * points[(i+3)%11][1]) / 2
            move_character(x, y)
+
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 
@@ -30,7 +40,10 @@ p4 = (-200,-200)
 
 size=10
 points = [(random.randint(-600,600),random.randint(-500,500)) for n in range(size)]
+
 while True:
- draw_curve_5_points(points,size)
+    draw_curve_5_points(points,size)
+
+
 
 close_canvas()
