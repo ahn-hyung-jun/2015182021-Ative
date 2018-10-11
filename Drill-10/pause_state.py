42bfb5a27c7fd1f02403288dc19da0cf2edc4518
ahn-hyung-jun(Ative) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-10-11 19:58:14 +0900

    Drill-10-1 끝
    키 입력은 SDLK_키
    였던것이다
    커밋횟수 1번

--- Drill-10/pause_state.py
+++ Drill-10/pause_state.py
@@ -1,5 +1,6 @@
 import game_framework
 from pico2d import *
+import main_state
 import title_state
 
 
@@ -34,8 +35,13 @@ def draw():
 
 def handle_events():
     events = get_events()
-    pass
-
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
 
 def pause(): pass
 
